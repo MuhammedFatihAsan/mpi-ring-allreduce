feaf0bda15f335aa2fdbae1ea55c5fa359a172c7
Furkan Dalfidan <mfurkandalfi@gmail.com>
2026-04-26 23:55:59 +0300

    Add utility headers

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,31 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-/* Shared helper code will be here. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+/**
+ * Bellek tahsisi (malloc) işlemlerini standartlaştıran fonksiyon. 
+ * Verilen boyutta bir float dizisi oluşturur.
+ */
+float* allocate_buffer(size_t size);
+
+/**
+ * Bellek boşaltma (free) işlemlerini standartlaştıran fonksiyon. 
+ */
+void free_buffer(float* data);
+
+/**
+ * Her Rank için tahmin edilebilir veriler (örneğin 1.0) üretir.
+ * Bu sayede toplama işleminin doğruluğu kolayca test edilebilir.
+ */
+void generate_dummy_data(float* data, size_t size);
+
+/**
+ * İşlemlerin doğruluğunu kontrol eden mekanizma.
+ * Örneğin 4 Node'lu sistemde sonucun 4.0 olup olmadığına bakar.
+ */
+int verify_results(float* data, size_t size, float expected_value);
 
 #endif
